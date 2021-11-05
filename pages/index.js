dfb030ff3d7f18e85af2e0fe4636738766a91bd1
Zeeshan Paracha <zparacha82@gmail.com>
2021-11-05 16:38:52 +0500

    feat: static site generate

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,28 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link';
+import Date from '../components/date';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
   return {
     props: {
       allPostsData
-    }
+    },
+    // revalidate: 60,
   }
 }
+
+// export async function getServerSideProps() {
+//   const allPostsData = getSortedPostsData()
+//   return {
+//     props: {
+//       allPostsData
+//     }
+//   }
+// }
+
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
@@ -29,11 +42,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
